03cbc3494112d8df28adef11e90a92bf17c61c23
pprg1996 <pprg1996@gmail.com>
2019-07-29 19:58:01 -0400

    Version inicial

---
+++ index.js
@@ -0,0 +1,36 @@
+let isInViewport = elem => {
+  let bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+let fadeDown = document.querySelectorAll(".fade-down");
+let fadeUp = document.querySelectorAll(".fade-up");
+
+console.log(fadeUp.length);
+
+addEventListener("scroll", () => {
+  fadeDown.forEach(elem => {
+    if (isInViewport(elem)) {
+      elem.classList.add("animated", "fadeInDown", "slow");
+      elem.style.visibility = "visible";
+    }
+
+    if (elem === document.querySelector(".section-title-2")) {
+      let serviciosTipos = document.querySelector(".servicios-tipos");
+      serviciosTipos.classList.add("animated", "fadeInUp", "slower");
+      serviciosTipos.style.visibility = "visible";
+    }
+  });
+
+  fadeUp.forEach(elem => {
+    if (isInViewport(elem)) {
+      elem.classList.add("animated", "fadeInUp", "slow");
+      elem.style.visibility = "visible";
+    }
+  });
+});
